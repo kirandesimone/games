1f2d335df59a4cf5b684e9ec0d7b92a01a84d5fa
kirandesimone <kirandesimone@gmail.com>
2023-08-02 16:12:22 -0400

    start: working with credo and dialyzer

--- lib/rock_paper_scissors.ex
+++ lib/rock_paper_scissors.ex
@@ -10,16 +10,25 @@ defmodule Games.RockPaperScissors do
 
   defp ai_chioce, do: Enum.random(["rock", "paper", "scissors"])
 
-  defp play_rps(player_1, player_2) do
+  @doc """
+  play_rps   fight logic
+  ## Examples
+
+    iex > play_rps("rock", "paper")
+    "You Win!"
+  """
+  @spec play_rps(String.t(), String.t()) :: :ok
+  def play_rps(player_1, player_2) do
     cond do
       player_1 == player_2 -> IO.puts("It's a tie!")
       player_1 == "rock" and player_2 == "scissors" -> IO.puts("You Win!")
       player_1 == "paper" and player_2 == "rock" -> IO.puts("You Win!")
       player_1 == "scissors" and player_2 == "paper" -> IO.puts("You Win!")
-      true -> IO.puts("You lose!")
+      true -> IO.puts("You Lose!")
     end
   end
 
+  @spec play :: :ok
   def play do
     player_1 = player_choice()
     player_2 = ai_chioce()
