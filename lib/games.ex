ec5278be214319563b23faf95e5b9e6c5a7436f9
kirandesimone <kirandesimone@gmail.com>
2023-08-03 12:59:59 -0400

    add: escript and game menu

--- lib/games.ex
+++ lib/games.ex
@@ -1,18 +1,28 @@
 defmodule Games do
   @moduledoc """
-  Documentation for `Games`.
+  ## Executable
+    Game menu for player to choose a game
   """
+  def main(_args) do
+    player_choice =
+      IO.gets(~s"""
+      What game would you like to play?
+      1. Guessing Game
+      2. Rock Paper Scissors
+      3. Wordle
 
-  @doc """
-  Hello world.
+      enter "stop" to exit
 
-  ## Examples
+      """)
+      |> String.downcase()
+      |> String.trim("\n")
 
-      iex> Games.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    case player_choice do
+      "guessing game" -> Games.GuessingGame.play()
+      "rock paper scissors" -> Games.RockPaperScissors.play()
+      "wordle" -> Games.Wordle.play()
+      "stop" -> IO.puts("Thanks for playing!")
+      _ -> IO.puts(~s("#{player_choice}" is not available))
+    end
   end
 end
